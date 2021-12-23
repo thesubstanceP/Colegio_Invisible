4c1e659e027366018b234130e14a0c2e164af5b0
thesubstanceP <95580791+thesubstanceP@users.noreply.github.com>
2021-12-22 19:03:22 -0500

    Update aes_decrypt.py
    bilingüe

--- scripts/aes_decrypt.py
+++ scripts/aes_decrypt.py
@@ -1,4 +1,19 @@
 ##################################################################################
+# Este guión descifrará un archivo protegido por una password AES 
+# La clave AES es el SHA256 hash del password hecho por el usuario
+#
+# Iniciar en el terminal usando el siguiente
+# > python aes_decrypt.py <PASSWORD> <CIPHERTEXT_PATH> <PLAINTEXT_PATH> 
+# <PASSWORD>: password usado para generar la clave AES 
+# <CIPHERTEXT_PATH>: el camino para llegar al archivo cifrado que va a estar descifrado
+# <PLAINTEXT_PATH>: el camino para llegar al nuevo archivo generado que estará descifrado 
+#
+#EJEMPLO:
+#>python aes_decrypt.py Password123 cipher.aes dec_file.jpg
+#
+# Sí <PASSWORD> NO ESTá INCLUIDO EL USUARIO ESTARá APUNTADA A INGRESARLO DE UNA MANERA SEGURA
+##################################################################################
+##################################################################################
 # this script will decrypt an AES password protected file
 # the AES key is the SHA256 hash of the user provided password
 #
@@ -26,11 +41,11 @@ if len(argv)==4:
     _, password, ciphertext_path, plaintext_path = argv
 elif len(argv)==3:
     _, ciphertext_path, plaintext_path   = argv
-    password = getpass.getpass("Input password for decryption: ")
+    password = getpass.getpass("Input password for decryption: " "Ingresar password para descifración)
 else:
-    print('Incorrect number of arguments. 2 or 3 expected')
+    print('Incorrect number of arguments. 2 or 3 expected''Numero incorrecto de argumentos. 2 o 3 esperado')
     print('> python aes_decrypt.py <PASSWORD> <CIPHERTEXT_PATH> <PLAINTEXT_PATH> ')
-    print('IF <PASSWORD> IS NOT INCLUDED USER WILL BE PROMPTED TO ENTER IT SECURELY')
+    print('IF <PASSWORD> IS NOT INCLUDED USER WILL BE PROMPTED TO ENTER IT SECURELY''Sí <PASSWORD> NO ESTá INCLUIDO EL USUARIO ESTARá APUNTADA A INGRESARLO DE UNA MANERA SEGURA')
     exit()
 
 def aes_decrypt_file(cipherfile_path,extractfile_path,password):
@@ -41,7 +56,7 @@ def aes_decrypt_file(cipherfile_path,extractfile_path,password):
 
 try:
     aes_decrypt_file(ciphertext_path,plaintext_path,password)
-    print('Success:',ciphertext_path, 'decryption complete' )
+    print('Success, exitoso:',ciphertext_path, 'decryption complete, descifración terminada' )
     print('Written to:', plaintext_path)
 except Exception as E:
-    print('Decryption failed',E)
+    print('Decryption failed, descifración fallada',E)
